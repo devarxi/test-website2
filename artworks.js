3ba70e8792daf43557db24dcae630b9304d14422
devarxi <devarshisabnis@gmail.com>
2026-03-06 14:18:04 +0530

    made a new js file and shifted all the artworks over there

---
+++ artworks.js
@@ -0,0 +1,291 @@
+const artworks = [
+    // Sketches
+    {
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch1.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch2.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch3.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch4.jpg"],
+      size: "Customized by style and size",
+      featured: true,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch5.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch6.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch7.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch8.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch9.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch10.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch11.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "",
+      genre: "sketches",
+      images: ["images/art/sketch12.jpg"],
+      size: "Customized by style and size",
+      featured: false,
+    },
+    {
+
+      title: "",
+      description: "a sketch reflects a perfect happy couple",
+      genre: "sketches",
+      images: ["images/art/sketch13.jpg", "images/art/sketch13_2.jpg"],
+      size: "Customized by style a-nd size",
+      featured: false,
+    },
+
+    // Wall Pieces
+    {
+
+      title: "Leaf n Line.",
+      description: "Geomatric Design, With Monstera & Banana Leaves.",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece1.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Saraswati Kala.",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece2.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Lippan Lipi",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece3.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Haryali Trio",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece4.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Divine Masculine",
+      description: "An art work showcasing the blend of 'lord Krishna & lord shiva'.",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece5.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Pankhi Trio",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece6.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Midas Touch",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece7.jpg", "images/art/wallpiece7_2.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Collector's Edition Leaf Fossil(Set of 3)",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece9.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Kala :- Make Your Customized Name Plates.",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece8.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+
+      title: "Meditating Buddha.",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece10.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: false,
+    },
+    {
+      title: "Kashtabhanjan hanumanji Maharaj 🚩",
+      description: "",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece11.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Lippan Luxe",
+      description: "A luxurious take on traditional Lippan art, featuring intricate patterns and metallic accents.",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece12.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Saptlakshmi Chinha",
+      description: "Saptlakshmi Chinha is a sacred symbol representing the seven forms of Goddess Lakshmi, each blessing a different aspect of life. Together, they signify complete prosperity, balance, and well-being.",
+      genre: "wallpieces",
+      images: ["images/art/wallpiece13.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+
+    // Other Art (Conceptual Interior)
+    {
+
+      title: "Play Palette",
+      description: "",
+      genre: "other",
+      images: ["images/art/conc_int1.jpg", "images/art/conc_int1_2.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Crescent Dreams",
+      description: "",
+      genre: "other",
+      images: ["images/art/conc_int2.jpg", "images/art/conc_int2_2.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Intertwined Devotion",
+      description: "",
+      genre: "other",
+      images: ["images/art/conc_int3.jpg", "images/art/conc_int3_2.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Peacock Palette",
+      description: "A conceptual artpiece made with vision of depecting",
+      genre: "other",
+      images: ["images/art/conc_int4.jpg", "images/art/conc_int4_2.jpg", "images/art/conc_int4_3.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Whispers of Gold",
+      description: "Whispers of gold is where white space becomes the canvas and gold becomes the voice. The piece reflects simplicity, refinement, and the power of understated detail, making it a calm yet striking presence in modern interiors.",
+      genre: "other",
+      images: ["images/art/conc_int5.jpg", "images/art/conc_int4_2.jpg", "images/art/conc_int4_3.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+    {
+
+      title: "Yanfeng employee recognition wall",
+      description: "A symbol of appreciation for the efforts, ideas, and integrity of our employees. It reflects our belief that success is built by people who are valued, respected, and recognized.",
+      genre: "other",
+      images: ["images/art/conc_int6.jpg"],
+      size: "Customized by style and size | Handcrafted",
+      featured: true,
+    },
+  ];
